e62a1cbd097ed9696b70745e8dbc01efe6db9c96
Travis Athougies <travis@athougies.net>
2019-01-27 14:02:08 -0800

    Fix up reference to hydra-app-jobsets

--- jobsets.nix
+++ jobsets.nix
@@ -1,4 +1,4 @@
-import <intrustd/nix/hydra-app-jobsets> {
+import <intrustd/nix/hydra-app-jobsets.nix> {
   description = "Intrustd Admin App";
   src = { type = "git"; value = "git://github.com/kitecomputing/photos.git"; emailresponsible = true; };
 }
