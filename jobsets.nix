a3b5c47d325ae25251b4ab7b32613678fe13b38a
Travis Athougies <travis@athougies.net>
2019-01-27 14:00:30 -0800

    Use explicit jobsets.nix in admin app

---
+++ jobsets.nix
@@ -0,0 +1,4 @@
+import <intrustd/nix/hydra-app-jobsets> {
+  description = "Intrustd Admin App";
+  src = { type = "git"; value = "git://github.com/kitecomputing/photos.git"; emailresponsible = true; };
+}
